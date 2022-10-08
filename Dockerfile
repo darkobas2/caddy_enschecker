fca8760dc89d3ed090e491b613887c3e0def63d0
Marko Man <darkobas@gmail.com>
2022-10-08 20:40:23 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.8-alpine3.16
+
+WORKDIR /app
+RUN apk add --no-cache gcc musl-dev
+RUN python -m pip install --upgrade pip
+
+RUN pip3 install web3
+
+COPY app.py .
+USER nobody
+EXPOSE 80
+
+ENTRYPOINT ["python3", "./app.py"]
---
+++ app.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time, sys, getopt
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
+
+from web3 import Web3, HTTPProvider
+from ens import ENS
+
+# Remove 1st argument from the
+# list of command line arguments
+argumentList = sys.argv[1:]
+
+# Options
+options = "hpb:"
+# Long options
+long_options = ["help", "provider", "basedomain"]
+
+try:
+    # Parsing argument
+    arguments, values = getopt.getopt(argumentList, options, long_options)
+    # checking each argument
+    for currentArgument, currentValue in arguments:
+ 
+        if currentArgument in ("-h", "--help"):
+            print ("OPTIONS: -b .example.com -p http://rpc.com")
+
+        elif currentArgument in ("-p", "--provider"):
+            userProvider = sys.argv[4]
+            print ("provider: ", userProvider)
+
+        elif currentArgument in ("-b", "--basedomain"):
+            domLen = len(currentValue)
+            baseDom = currentValue
+            print ("basedomain: ", baseDom)
+
+except getopt.error as err:
+    # output error, and return with an error code
+    print (str(err))
+
+w3 = Web3(Web3.HTTPProvider(userProvider))
+ns = ENS(w3)
+
+
+hostName = "0.0.0.0"
+serverPort = 80
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_path = urlparse(self.path)
+        domain = parse_qs(urlparse(self.path).query).get('domain', None)
+        if domain is not None:
+          if domain[0].endswith(baseDom):
+            addr = w3.ens.address(domain[0].replace(baseDom, '') + ".eth")
+            print (addr)
+          else:
+            addr = None
+
+          if (addr != None):
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("OK", "utf-8"))
+          else:
+            self.send_response(403)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("None", "utf-8"))
+        else:
+          self.send_response(404)
+          self.send_header("Content-type", "text/html")
+          self.end_headers()
+          self.wfile.write(bytes("Err", "utf-8"))
+ 
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
